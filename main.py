07f6460a36bf607e8592f4081e685c5927345c82
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-31 13:10:48 +0200

    Add weather dictionary example and print statements in main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+﻿
+weather: dict = {'time': '12:00',
+                 'weather': {'morning': 'rain',
+                             'evening': 'more rain'}}
+
+print(weather['time'])
+print(weather['weather']['morning'])
+print(weather['weather']['evening'])
