0a93a31ffa6bdf5e720a59eadd0043cb2210b040
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-31 13:13:35 +0200

    Update main.py: add user dictionary manipulation example with print statements

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ weather: dict = {'time': '12:00',
                  'weather': {'morning': 'rain',
                              'evening': 'more rain'}}
 
+
+users: dict = {1: 'Bob', 2: 'Luigi'}
+print(users)
+del users[2]
+users[1] = 'James'
+print(users)
+users.pop(2)
+print(users)
+
 print(weather['time'])
 print(weather['weather']['morning'])
 print(weather['weather']['evening'])
